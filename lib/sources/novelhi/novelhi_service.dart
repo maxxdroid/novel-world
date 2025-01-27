a58321f70ed045a27faad106ff110ecd43006174
maxxdroid <theonlymaxmc@gmail.com>
2025-01-27 03:41:42 -0800

    get novel hi chapters

--- lib/sources/novelhi/novelhi_service.dart
+++ lib/sources/novelhi/novelhi_service.dart
@@ -1,10 +1,15 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
+import 'package:html/dom.dart';
 import 'package:http/http.dart' as http;
 import 'package:html/parser.dart' show parse;
+import 'package:novel_world/functions/novel_functions.dart';
+import 'package:novel_world/model/chapter.dart';
 import 'package:novel_world/model/novel.dart';
 
 class NovelHiService {
+  final NovelFunction novelFunction = NovelFunction();
 
   static const url = "https://novelhi.com/";
   Map<String, String> headers = {
@@ -47,7 +52,7 @@ class NovelHiService {
 
           Novel newNovel = Novel(
             title: novel["bookName"],
-            link: "${url}s/${title.replaceAll(" ", "-")}",
+            link: "${url}s/${title.replaceAll(" ", "-").replaceAll("'", "").replaceAll(":", "")}",
             imgUrl: novel["picUrl"],
             author: novel["authorName"],
             status: novel["bookStatus"] == '0' ? "completed" : "ongoing",
@@ -65,6 +70,66 @@ class NovelHiService {
     }
     
     return getNovels;
+  }
+
+  Future<Novel> getChapters (Novel novel) async {
+    List<Chapter> chapters = [];
+
+    String url = novelFunction.getNovelHiUrl(novel.link ?? "");
+
+    print(url);
+
+    try {
+      final response = await http.get(Uri.parse(url), headers: headers);
+
+      if (response.statusCode == 200) {
+        // Log raw HTML for debugging
+        if (kDebugMode) {
+          print("HTML Content: ${response.body}");
+        }
+
+        // Parse the HTML response
+        final Document doc = parse(response.body);
+
+        // Extract chapter elements
+        final List<Element> chapterElements = doc.querySelectorAll('.dirList ul li a span');
+
+        if (chapterElements.isEmpty) {
+          if (kDebugMode) {
+            print("No chapters found. Check your selector or HTML structure.");
+          }
+        } else {
+          // Extract chapter text
+          for (final element in chapterElements) {
+            final chapterText = element.text.trim();
+            if (kDebugMode) {
+              print("Chapter: $chapterText");
+            }
+
+            // Extract numbers using regex
+            final match = RegExp(r'\d+').firstMatch(chapterText);
+            final number = match != null ? int.parse(match.group(0)!) : null;
+            if (kDebugMode) {
+              print("Extracted Number: $number");
+            }
+
+            Chapter chapter =Chapter(
+                number: number ?? 0,
+                title: chapterText,
+                link: "${novel.link}/$number"
+            );
+            chapters.add(chapter);
+          }
+        }
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+    novel.chapters = chapters;
+
+    return novel;
   }
 
   Future<List<Novel>> searchNovels (String keyWords, int page) async {
@@ -111,7 +176,9 @@ class NovelHiService {
       }
 
     } catch (e) {
-      print(e);
+      if (kDebugMode) {
+        print(e);
+      }
     }
 
     return getNovels;
