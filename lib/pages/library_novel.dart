6c6180c7f7e244cd5498a71e7d1a842130147192
maxxdroid <theonlymaxmc@gmail.com>
2025-01-27 08:37:31 -0800

    Library home bet novel hi and novel bin

--- lib/pages/library_novel.dart
+++ lib/pages/library_novel.dart
@@ -4,6 +4,8 @@ import 'package:novel_world/controllers/library_controller.dart';
 import 'package:novel_world/model/novel.dart';
 import 'package:novel_world/widgets/novel_details.dart';
 
+import '../sources/novelhi/views/novel_hi_offline_details.dart';
+
 class LibraryNovel extends StatefulWidget {
   final int index;
   const LibraryNovel({super.key, required this.index});
@@ -20,7 +22,11 @@ class _LibraryNovelState extends State<LibraryNovel> {
     return Scaffold(
       body: Obx(() {
         Novel novel = libraryController.novels[widget.index];
-        return NovelDetailsWidget(novel: novel);
+        if (novel.source == "Novel Bin") {
+          return NovelDetailsWidget(novel: novel);
+        } else {
+          return NovelHiOffline(novel: novel);
+        }
       }),
     );
   }
