579a6262f546015361369ef1654a7290b5bc9ac9
maxxdroid <theonlymaxmc@gmail.com>
2025-02-22 08:21:34 -1200

    feat: integrate realm plugin across platforms
    Integrates the Realm plugin into the Flutter project for Linux, macOS, and Windows platforms. This includes registering the Realm plugin with the Flutter plugin registry on each platform.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import realm
 import shared_preferences_foundation
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  RealmPlugin.register(with: registry.registrar(forPlugin: "RealmPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
 }
