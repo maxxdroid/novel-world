af3f246d5f4048a25c9854ce6c6042656e06ca85
maxxdroid <theonlymaxmc@gmail.com>
2025-02-23 07:01:25 -1200

    refactor: remove unused chapter model
    The `Chapter` class in `chapter.dart` is commented out, effectively removing it from the codebase. This change removes the unused model.

--- lib/model/chapter.dart
+++ lib/model/chapter.dart
@@ -1,49 +1,49 @@
-class Chapter {
-  String? title;
-  bool read;
-  String link;
-  String? content;
-  String? date;
-  String? next;
-  String? previous;
-  int number;
-  String? book;
-  bool downloaded;
-
-  Chapter({
-    this.title,
-    this.content,
-    this.next,
-    this.previous,
-    this.book,
-    this.date,
-    required this.number,
-    bool? read,
-    required this.link
-}) : read = read ?? false, downloaded = content != null;
-
-  factory Chapter.fromJson(Map<String, dynamic> json) {
-    return Chapter(
-      content: json["content"],
-        link: json["link"],
-      title: json["title"],
-      read: json["read"],
-      next: json["next"],
-      previous: json["prev"],
-      number: json["number"]
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      "title": title,
-      "read" : read,
-      "link" : link,
-      "next" : next,
-      "prev" : previous,
-      "content" : content,
-      "date" : date,
-      "number" : number
-    };
-  }
-}
+// class Chapter {
+//   String? title;
+//   bool read;
+//   String link;
+//   String? content;
+//   String? date;
+//   String? next;
+//   String? previous;
+//   int number;
+//   String? book;
+//   bool downloaded;
+//
+//   Chapter({
+//     this.title,
+//     this.content,
+//     this.next,
+//     this.previous,
+//     this.book,
+//     this.date,
+//     required this.number,
+//     bool? read,
+//     required this.link
+// }) : read = read ?? false, downloaded = content != null;
+//
+//   factory Chapter.fromJson(Map<String, dynamic> json) {
+//     return Chapter(
+//       content: json["content"],
+//         link: json["link"],
+//       title: json["title"],
+//       read: json["read"],
+//       next: json["next"],
+//       previous: json["prev"],
+//       number: json["number"]
+//     );
+//   }
+//
+//   Map<String, dynamic> toJson() {
+//     return {
+//       "title": title,
+//       "read" : read,
+//       "link" : link,
+//       "next" : next,
+//       "prev" : previous,
+//       "content" : content,
+//       "date" : date,
+//       "number" : number
+//     };
+//   }
+// }
